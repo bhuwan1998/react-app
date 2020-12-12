daaef853e0d9aa521197d05c9d0e7226a962247b
bhuwan1998 <rfperfect785@gmail.com>
2020-12-12 23:06:48 +1100

    react components, props, state shift up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap'; // importing first component from react strap 
 import Menu from './components/MenuComponent';
+import './App.css';
+import { DISHES } from './shared/dishes';
 
 
 class App extends Component{
+
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      dishes: DISHES
+    };
+
+  }
+
+
   render(){
     return(
       <div>
@@ -12,7 +25,7 @@ class App extends Component{
             <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
           </div>
         </Navbar> 
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
