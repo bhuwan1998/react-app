e1f6ddb6eefa0985b2f1027c0bb4b5bb0240cf3f
bhuwan1998 <rfperfect785@gmail.com>
2021-01-11 11:29:54 +1100

    Redux Form reset

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import { connect } from 'react-redux';
 import {addComment, fetchDishes} from '../redux/ActionCreaters'; // we need this to add an action JS object which we can then 
 // dispatch to the store 
 // by calling store dispatch 
+import {actions} from 'react-redux-form';
 
 
 
@@ -27,7 +28,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)), 
-  fetchDishes: () => {dispatch(fetchDishes())} // fetch dishes when called and store in Redux store
+  fetchDishes: () => {dispatch(fetchDishes())},  // fetch dishes when called and store in Redux store
+  resetFeedbackForm: () => { dispatch (actions.reset('feedback'))}
 });
 
 // the state becomes available to the main class through mapStateToProps as the name suggests 
@@ -82,7 +84,7 @@ class Main extends Component{
             <Route path="/home" component={HomePage} />
             <Route exact path ="/menu" component={() => <Menu dishes={this.props.dishes} />}/>
             <Route path="/menu/:dishId" component={DishWithId} />
-            <Route exact path="/contactus" component={Contact} />
+            <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>} />
             <Route path="/aboutus" component={() => <About leaders ={this.props.leaders}/>}/>
             <Redirect to="/home" />
           </Switch> 
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import {Dishes} from './dishes'; 
+import {createForms} from 'react-redux-form';
 import {Comments} from './comments'; 
 import {Promotions} from './promotions'; 
 import {Leaders} from './leaders'; 
 import thunk from 'redux-thunk'; 
 import logger from 'redux-logger'; 
+import { InitialFeedback } from './forms';
 
 
 export const ConfigureStore = () => {
@@ -13,9 +15,14 @@ export const ConfigureStore = () => {
             dishes: Dishes, 
             comments: Comments, 
             promotions: Promotions, 
-            leaders: Leaders // combineReducers will map it to various properties 
+            leaders: Leaders, // combineReducers will map it to various properties 
             // recomposed the global state 
-            // overall state of the application 
+            // overall state of the application
+            ...createForms({
+                feedback: InitialFeedback
+            })  // adds in necessary reducer functions to the state information into our create store
+            // we don't need to write our own reducers 
+            
         }), 
         applyMiddleware(thunk, logger)
 
